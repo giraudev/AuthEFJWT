e46df4de7acc531547b7039668b43a787568fbee
Giraudev <giraudev@gmail.com>
2018-02-09 19:32:32 -0200

    JWT

--- Repositorio/CodeFirstBanco.cs
+++ Repositorio/CodeFirstBanco.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using modelobasicoefjwt.Models;
 
@@ -12,13 +13,17 @@ namespace modelobasicoefjwt.Repositorio
             var usuario = new Usuario(){
                 Nome = "Fernando",
                 Email = "fernando.guerra@corujasdev.com.br",
-                Senha = "123456"
+                Senha = "123456", 
+                //dados colocados posteriormente para fazer updates
+                DataNascimento = Convert.ToDateTime("10-10-1978"),
+                Cpf = "339.265.568-82"
             };
 
             contexto.Usuarios.Add(usuario);
 
             var permissao = new Permissao(){
-                Nome = "Financeiro"
+                //nome alterado de Controle
+                Nome = "Converso"
             };
 
             contexto.Permissoes.Add(permissao);
